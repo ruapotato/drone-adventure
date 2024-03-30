97e7d60d70bad64fcdadfad6ea2ae097f7726604
David Hamner <david.hamner@puri.sm>
2024-03-29 21:42:45 -0700

    better drone sounds

--- drone_sound.gd
+++ drone_sound.gd
@@ -8,7 +8,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pitch_scale = abs(drone.throttle/5) + 1.2
+	pitch_scale = abs(drone.throttle/8) + .5
 
 
 func _on_finished():
